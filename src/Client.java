41130bf4f49af486e3be6c672b7de5b45c9f8c10
Vickz84259 <vickz84259@gmail.com>
2017-03-02 21:41:14 +0300

    Remove getters and setters
    The getters and setters in the Client.java file were unnecessary.
    This is because the private variables are only accessed from within the Class.
    Hence, there's no need to use the getters and setters as the functions in
    the class can easily access the variables without them.
    
    Signed-off-by: Vickz84259 <vickz84259@gmail.com>

--- src/Client.java
+++ src/Client.java
@@ -9,81 +9,29 @@ public class Client {
     public static final double LIMIT = 10000;// the minimum amount an account can have
     private static double balance;
     private static double deposit;
-    private static Scanner userInput = new Scanner(System.in);
-    private static Client client = new Client();
-    //private static Bank ba = new Bank();
+    private static Scanner userInput;
     private String firstName;
     private String lastName;
     private int accountNumber;
 
     public Client(){
-
-    }
-
-    //This constructor will be used to pass data into an object to store the data to a file
-    public Client(int acctNo, String first, String last, double bal) {
-        setFirstName(first);
-        setAccountNumber(acctNo);
-        setLastName(last);
-        setBalance(bal);
+        userInput = new Scanner(System.in);
     }
 
     //method to deposit money into account
     public static void deposit() {
         System.out.println("Please enter the amount you want to deposit");
         deposit = userInput.nextDouble();
-        client.setDeposit(deposit);
         balance += deposit;
-        client.setBalance(balance);
-        System.out.println("Transaction completed\nAmount deposited: Ksh " + client.getDeposit());
+        System.out.println("Transaction completed\nAmount deposited: Ksh " + deposit);
         checkBalance();
     }
 
     //pretty self explanatory method
     public static void checkBalance() {
-        System.out.println("Your account balance is: Ksh " + client.getBalance() + "\n");
+        System.out.println("Your account balance is: Ksh " + balance + "\n");
     }
 
-    //getter and setter methods
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(int accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-    public double getDeposit() {
-        return deposit;
-    }
-
-    public void setDeposit(double deposit) {
-        this.deposit = deposit;
-    }
 
     //This method handles basic user info and creates an account for them
     public void createAccount() {
@@ -92,16 +40,13 @@ public class Client {
         System.out.println("Provide a few details to enable account creation priss");
         System.out.println("First name:");
         firstName = userInput.next();
-        client.setFirstName(firstName);
 
         System.out.println("Last name:");
         lastName = userInput.next();
-        client.setLastName(lastName);
 
         accountNumber = (int) (Math.random() * 1000 + 1);//a random 3-digit number is chosen as the account number
-        client.setAccountNumber(accountNumber);
 
-        System.out.println("Thank you. Your account number is : " + client.getAccountNumber());
+        System.out.println("Thank you. Your account number is : " + accountNumber);
 
     }
 
@@ -115,7 +60,6 @@ public class Client {
             System.out.println("Sorry, withdrawal limit exceeded");
             balance += withdrawal;
         } else {
-            client.setBalance(balance);
             System.out.println("Transaction completed\nAmount withdrawn: Ksh " + withdrawal);
         }
         checkBalance();
