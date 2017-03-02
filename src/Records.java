791f87e3349701fbcaa0813457d84cfcb4c05dad
Vickz84259 <vickz84259@gmail.com>
2017-03-03 00:23:57 +0300

    New Functionality in Records.java
    As part of restructuring the Records class, I have added code to offer a
    different path for a new user creating an account and an existing user.
    This lays the ground work for more features to come later.
    
    Signed-off-by: Vickz84259 <vickz84259@gmail.com>

--- src/Records.java
+++ src/Records.java
@@ -5,11 +5,38 @@ import java.util.Scanner;
 /**
  * Created by eric on 21/10/16.
  */
-//This is the class that deals with storing the user's(s') data to a text file
-public class Records extends Client {
-    private static Client client = new Client();
+//This is the class that deals with storing the user's data to a text file
+public class Records {
     private Formatter formatter;
+    private Scanner userInput;
     private Scanner input;
+    private Client client;
+
+    public Records(Scanner input){
+        userInput = input;
+        client = new Client(userInput)
+        System.out.println("Welcome user.");
+
+        do{
+            System.out.println("Select an option:" +
+                "\n1.New User" +
+                "\n2.Existing User");
+            try{
+                switch (userInput.nextInt()){
+                    case 1:
+                        client.createAccount();
+                        break;
+                    case 2:
+                        getUserRecord();
+                        break;
+                }
+            break;
+            } catch(InputMismatchException e){
+                System.err.println("Please choose between option 1 and 2.");
+                continue;
+            }
+        }while(true);
+    }
 
     //This opens the file and enables it for data entry
     public void loadFile() {
